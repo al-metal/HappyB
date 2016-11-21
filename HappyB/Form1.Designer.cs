0d54bc3e9b6c7181b73dbcf8d3a2e89f1ab09d73
al-metal <al-metal@bk.ru>
2016-11-21 14:34:14 +0400

    Вывод данных на сегодняшний день

--- HappyB/Form1.Designer.cs
+++ HappyB/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,11 +59,22 @@
             this.выходToolStripMenuItem.Text = "Выход";
             this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(275, 385);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
@@ -81,6 +93,7 @@
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HappyB/Form1.cs
+++ HappyB/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,13 @@ namespace HappyB
 {
     public partial class Form1 : Form
     {
+        string date = DateTime.Now.AddDays(+1).ToString("dd.MM.yyyy");
+
         public Form1()
         {
             InitializeComponent();
         }
-
+        #region Обработка формы
         private void Form1_Deactivate(object sender, EventArgs e)
         {
             if (WindowState == FormWindowState.Minimized)
@@ -50,6 +53,37 @@ namespace HappyB
         {
             WindowState = FormWindowState.Minimized;
             e.Cancel = true;           
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> actionsShow = new List<string>();
+
+            string[] actions = File.ReadAllLines("hb.txt", Encoding.GetEncoding(1251));
+            foreach(string str in actions)
+            {
+                string[] actionStr = str.Split(';');
+                string nameActions = actionStr[0];
+                string dateActions = actionStr[1];
+                if (dateActions.Remove(5) == date.Remove(5))
+                    actionsShow.Add(str);
+            }
+            if(actionsShow.Count != 0)
+            {
+                string reliseStr = "";
+                foreach (string str in actionsShow)
+                {
+                    string[] actionStr = str.Split(';');
+                    string nameActions = actionStr[0];
+                    string dateActions = actionStr[1];
+
+                    reliseStr += nameActions;
+                }
+                MessageBox.Show("Сегодня мероприятие у \n" + reliseStr);
+            }
+                
         }
     }
 }
